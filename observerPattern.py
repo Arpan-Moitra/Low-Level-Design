bf2658a8069d56cf62220e5c28f4aa382350530d
moitra <2000arpan@gmail.com>
2024-02-25 13:49:08 +0530

    Initial Commit for LLD

---
+++ observerPattern.py
@@ -0,0 +1,10 @@
+"""
+Observer Design Pattern
+
+Problem:
+
+
+Use-case:
+    Implement Notify me functionality on E-commerce websites. If a product is out of stock, notify the
+    subscribed user once the product is back in stock.
+"""
